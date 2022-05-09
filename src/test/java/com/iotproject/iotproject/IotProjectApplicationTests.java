ab46317414412b002f09c956e23320bb9832f6ac
Malaka98 <malaka.aruna14413@gmail.com>
2022-05-09 23:59:42 +0530

    first commit

---
+++ src/test/java/com/iotproject/iotproject/IotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iotproject.iotproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
